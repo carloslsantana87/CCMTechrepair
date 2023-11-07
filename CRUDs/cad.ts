5a1cdbca4f3f1b39cbc8e562e9b4e9bd30ca9a1b
carloslsantana87 <139463823+carloslsantana87@users.noreply.github.com>
2023-11-07 15:27:16 -0300

    Atualizacao type

---
+++ CRUDs/cad.ts
@@ -0,0 +1,86 @@
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    porta: 587,
+    secure: true,
+    auth: {
+        user: "ccmtechrepairtechrepair@gmail.com",
+        pass: "juox rrrt rtzo rlul"
+    }
+});
+
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../config/config.js');
+const email = require('../config/email.js')
+const clienteModel = require('../models/clientes.js');
+const clientes = require('../models/clientes.js');
+const sequelize = new Sequelize(config.development);
+const clienteCad = clienteModel(sequelize, DataTypes);
+async function run() {
+
+    var nomeCli = "AMANDA COSMETICOS";
+    var tipoCli = "FISICA";
+    var cnpjCli = 99009898000922;
+    var cpfCli = 0;
+    var cepCli = 50770500;
+    var ruaCli = "Rua Francisco Porfirio";
+    var numeCLi = 175;
+    var complementoCli = "Apt 305";
+    var bairroCli = "Afogados";
+    var cidadeCli = "Recife";
+    var datadocadastroCli = 25 - 10 - 2023;
+    var emailCLi = "carloslsantana87@gmail.com";
+
+
+    try {
+        //Cadastro de Pedidos
+        const cadcliente = await clienteCad.create({
+            nome: nomeCli,
+            tipo: tipoCli,
+            cnpj: cnpjCli,
+            cpf: cpfCli,
+            cep: cepCli,
+            endereco: ruaCli,
+            numero: numeCLi,
+            complemento: complementoCli,
+            bairro: bairroCli,
+            cidade: cidadeCli,
+            datadocadastro: datadocadastroCli,
+            email: emailCLi
+        });
+        const cadastro = await clienteCad.findAll();
+        transporter.sendMail({
+            from: "CCM - Techrepair <ccmtechrepairtechrepair@gmail.com>",
+            to: emailCLi,
+            subject: "Teste",
+            text: `Você fez o seu cadastro na TecRephair
+                   Seus dados foram cadastrados: 
+                   Numero do cliente: 
+                   Nome do cliente: ${nomeCli}
+                   Endereço: ${ruaCli} ${numeCLi} ${complementoCli} ${bairroCli} ${cidadeCli} ${cepCli}
+                   cnpj: ${cnpjCli}
+                   cpf: ${cpfCli}`
+        }).then(message => {
+            console.log(message)
+        }).catch(err => {
+            console.log(err);
+        });
+        console.log('Cliente Cadastrado:', cadastro); //.map(p => p.toJSON()));
+
+    } catch (error) {
+        console.error('Erro: ', error.message);
+
+    } finally {
+        // Fechar conexão com banco de dados
+        await clienteCad.sequelize.close();
+    }
+    
+    /*(async () => {
+        const listadeclientes = await clientes.findByPk(1);
+    
+    console.log(listadeclientes);
+    });*/
+
+}
+run();
